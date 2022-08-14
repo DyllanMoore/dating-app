fd993b944a1177ac957f3251aae10b6cecea5a13
Sekhar Reddy <77301802+SekhReddy@users.noreply.github.com>
2022-08-13 20:51:11 -0400

    complete main animation

--- FrontendV2/src/app/app.component.ts
+++ FrontendV2/src/app/app.component.ts
@@ -1,10 +1,108 @@
 import { Component } from '@angular/core';
+import { loadFull } from 'tsparticles';
+import { Container } from 'tsparticles-engine/types/Core/Container';
+import { Engine } from 'tsparticles-engine/types/engine';
+import { MoveDirection } from 'tsparticles-engine/types/Enums/Directions/MoveDirection';
+import { ClickMode } from 'tsparticles-engine/types/Enums/Modes/ClickMode';
+import { HoverMode } from 'tsparticles-engine/types/Enums/Modes/HoverMode';
+import { OutMode } from 'tsparticles-engine/types/Enums/Modes/OutMode';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'FrontendV2';
+
+  id = 'tsparticles';
+
+  particlesOptions = {
+    background: {
+      color: {
+        value: '#110000',
+      },
+    },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: ClickMode.push,
+        },
+        onHover: {
+          enable: true,
+          mode: HoverMode.repulse,
+        },
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 100,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: '#ffffff',
+        opacity: 0.2,
+      },
+      links: {
+        color: '#ffffff',
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: MoveDirection.none,
+        enable: true,
+        outModes: {
+          default: OutMode.bounce,
+        },
+        random: true,
+        speed: 6,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        options: {
+          character: {
+            value: ['\u2764', '\u2661', '\u2661'], // the text to use as particles, any string is valid, for escaping unicode char use the `\uXXXX` synta
+          },
+        },
+        type: ['character'],
+      },
+
+      size: {
+        value: { min: 10, max: 20 },
+      },
+    },
+    detectRetina: true,
+  };
+
+  particlesLoaded(container: Container): void {
+    console.log(container);
+  }
+
+  async particlesInit(engine: Engine): Promise<void> {
+    console.log(engine);
+    await loadFull(engine);
+  }
 }
--- FrontendV2/src/app/app.module.ts
+++ FrontendV2/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgParticlesModule } from 'ng-particles';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, WelcomeComponent, LoginComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgParticlesModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
