bde4536f770a557507d669e4dc9196c03155743c
Sekhar Reddy <77301802+SekhReddy@users.noreply.github.com>
2022-08-14 03:43:20 -0400

    pls help

--- FrontendV2/src/app/app-routing.module.ts
+++ FrontendV2/src/app/app-routing.module.ts
@@ -1,10 +1,51 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+// import { ProfileComponent } from './components/profile/profile.component';
+// import { SignupComponent } from './components/signup/signup.component';
+// import { MingleComponent } from './components/mingle/mingle.component';
+// import { MatchesComponent } from './components/matches/matches.component';
+// import { UpdateComponent } from './components/update/update.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
 
-const routes: Routes = [];
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+
+  {
+    path: 'mingle',
+    component: MingleComponent,
+  },
+
+  {
+    path: 'matches',
+    component: MatchesComponent,
+  },
+
+  {
+    path: 'update',
+    component: UpdateComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
